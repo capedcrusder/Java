e5e42cfdeb1da223629c6bef79f3c61edeeb26fe
Shubham Yadav <imshubh19@gmail.com>
2024-10-17 16:49:25 +0530

    Variables & Data Types

---
+++ src/DataTypesVariables/DataVariables.java
@@ -0,0 +1,34 @@
+package DataTypesVariables;
+public class DataVariables {
+    public static void main(String[]args){
+//Integer
+int x=10;
+byte b=2;
+short s=1;
+long l= 12345667;
+
+//float 
+float f=1.1234f;    //by default java takes double vaule so we have to explicitly metion that we want to work with float
+double d=23.34567;
+
+//character : can be a number or cam be a alphabet
+char a='8';
+char g='f';
+
+//boolean : t/f
+
+boolean i=true;
+boolean j=false;
+
+System.out.println(x);
+System.out.println(b);
+System.out.println(s);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(a);
+System.out.println(g);
+System.out.println(i);
+System.out.println(j);
+    }
+}
